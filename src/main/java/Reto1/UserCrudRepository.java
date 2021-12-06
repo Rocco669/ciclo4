aa38908e0be873a9b354b9f5fea04537a3c46139
Ronald Gomez Poveda <91928804+Rocco669@users.noreply.github.com>
2021-12-06 15:20:19 -0500

    Add files via upload

---
+++ src/main/java/Reto1/UserCrudRepository.java
@@ -0,0 +1,11 @@
+package Reto1;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UserCrudRepository extends CrudRepository <User, Integer> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email,String password);
+
+}
